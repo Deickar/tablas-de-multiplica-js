cbed146009dbbddb1c8908d3bbea8a821028b8f7
Deickar <deickar@outlook.com>
2025-06-03 21:54:35 -0600

    tablas con operador de for loop

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// ejemplo basico
-var i = 1;
-console.log(i, "X i =", i * i);
-console.log(i, "X i =", i * 2);
-console.log(i, "X i =", i * 3);
-console.log(i, "X i =", i * 4);
-console.log(i, "X i =", i * 5);
-console.log(i, "X i = ", i * 6);
-console.log(i, "X i =", i * 7);
-console.log(i, "X i =", i * 8);
-console.log(i, "X i =", i * 9);
-console.log(i, "X i=", i * 10);
+// tabla de multiplica del 1
+const nuemero = 1;
+// ejemplo con for
+for (var i = 1; i <= 10; i++) {
+  console.log(nuemero, "X i =", nuemero * i);
+}
