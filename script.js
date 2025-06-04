7cadcde30d85eb40fd60250129a87ae59d7a9c5a
Deickar <deickar@outlook.com>
2025-06-03 21:42:33 -0600

    se hagrego un ejemplo de tablas multiplicar

---
+++ script.js
@@ -0,0 +1,12 @@
+// ejemplo basico
+var i = 1;
+console.log(i, "X i =", i * i);
+console.log(i, "X i =", i * 2);
+console.log(i, "X i =", i * 3);
+console.log(i, "X i =", i * 4);
+console.log(i, "X i =", i * 5);
+console.log(i, "X i = ", i * 6);
+console.log(i, "X i =", i * 7);
+console.log(i, "X i =", i * 8);
+console.log(i, "X i =", i * 9);
+console.log(i, "X i=", i * 10);
