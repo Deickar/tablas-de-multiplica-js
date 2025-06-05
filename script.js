e64fa326c4002f8acde5ba40478a79c64c0a35d4
mativale0207 <mativale0207@gmail.com>
2025-06-04 20:13:06 -0600

    se agrega tabla del 9 y tabla del 10

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 // tabla de multiplica del 1
-let numero1 = 1;
+let nuemero1 = 1;
 console.log("Tabla del 1");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(numero1, "x", i, "=", numero1 * i);
+  console.log(nuemero1, "x", i, "=", nuemero1 * i);
 }
 // salto de linea
 console.log("\n");
 
 //tabla de multiplica del 2
-let numero2 = 2;
+let nuemero2 = 2;
 console.log("Tabla del 2");
 // ejemplo con for
 for (let i = 1; i <= 10; i++) {
-  console.log(numero2, "x", i, "=", numero2 * i);
+  console.log(nuemero2, "x", i, "=", nuemero2 * i);
 }
-// salto de linea
-console.log("\n");
 
 // tabla de multiplica del 3
 let nuemero3 = 3;
 console.log("Tabla del 3");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero3, "x", i, "=", nuemero3 * i);
+  console.log(nuemero3, "x", i, "=", nuemero1 * i);
 }
 // salto de linea
 console.log("\n");
@@ -33,47 +31,27 @@ let nuemero4 = 4;
 console.log("Tabla del 4");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero4, "x", i, "=", nuemero4 * i);
+  console.log(nuemero4, "x", i, "=", nuemero1 * i);
 }
 // salto de linea
 console.log("\n");
 
-//tabla de multiplica del 5
-let numero5 = 5;
-console.log("Tabla del 5");
-// ejemplo con for
+//tabla de multiplicar del 9
+let numero9 = 9;
+console.log("Tabla del 9");
+//utilizamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(numero5, "x", i, "=", numero5 * i);
+  console.log(numero9, "x", i, "=", numero9 * i);
 }
-// salto de linea
-console.log("\n");
+//salto de linea
+console.log("/n");
 
-//tabla de multiplica del 6
-let numero6 = 6;
-console.log("Tabla del 6");
-// ejemplo con for
+//tabla de multiplicar del 10
+let numero10 = 10;
+console.log("Tabla del 10");
+//utilizamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(numero6, "x", i, "=", numero6 * i);
+  console.log(numero10, "x", i, "=", numero10 * i );
 }
-// salto de linea
-console.log("\n");
-
-//tabla de multiplicar del 7
-let numero7 = 7;
-console.log("Tabla del 7");
-// ejemplo con for
-for (let i = 1; i <= 10; i++) {
-  console.log(numero7, "x", i, "=", numero7 * i);
-}
-// salto de linea
-console.log("\n");
-
-//tabla de multiplicar del 8
-let numero8 = 8;
-console.log("Tabla del 8");
-// ejemplo con for
-for (let i = 1; i <= 10; i++) {
-  console.log(numero8, "x", i, "=", numero8 * i);
-}
-// salto de linea
-console.log("\n");
+//salto de linea
+console.log("/n");
