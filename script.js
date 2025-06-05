caec3c30936e45465626350b2636251a648a89e2
Deickar <deickar@outlook.com>
2025-06-04 19:00:06 -0600

    se hagre la  un salto linea se otras liena para mas orden

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // tabla de multiplica del 1
-let nuemero = 1;
+let nuemero1 = 1;
+console.log("Tabla del 1");
+// utilzamos el bucle for para hacer la tabla
+for (let i = 1; i <= 10; i++) {
+  console.log(nuemero1, "x", i, "=", nuemero1 * i);
+}
+// salto de linea
+console.log("\n");
+
+//tabla de multiplica del 2
+let nuemero2 = 2;
+console.log("Tabla del 2");
 // ejemplo con for
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero, "x", i, "=", nuemero * i);
+  console.log(nuemero2, "x", i, "=", nuemero2 * i);
 }
