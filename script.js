9bfa4b86ab741748ab43f759763930d229a5d8a0
Deickar <deickar@outlook.com>
2025-06-04 18:50:50 -0600

    se arreglo output de las salidas la tabla 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // tabla de multiplica del 1
-const nuemero = 1;
+let nuemero = 1;
 // ejemplo con for
-for (var i = 1; i <= 10; i++) {
-  console.log(nuemero, "X i =", nuemero * i);
+for (let i = 1; i <= 10; i++) {
+  console.log(nuemero, "x", i, "=", nuemero * i);
 }
