91f6f1b3d9dbc1505aeec214f4a5feb1ded634ec
Slopez5844 <present210@gmail.com>
2025-06-04 19:05:45 -0600

    agregada la tabla del 3 y cuatro, se copio el codigo original y se cambio la variable numero por la tabla que corresponde.

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ console.log("Tabla del 2");
 for (let i = 1; i <= 10; i++) {
   console.log(nuemero2, "x", i, "=", nuemero2 * i);
 }
+
+// tabla de multiplica del 3
+let nuemero3 = 3;
+console.log("Tabla del 3");
+// utilzamos el bucle for para hacer la tabla
+for (let i = 1; i <= 10; i++) {
+  console.log(nuemero3, "x", i, "=", nuemero1 * i);
+}
+// salto de linea
+console.log("\n");
+
+// tabla de multiplica del 4
+let nuemero4 = 4;
+console.log("Tabla del 4");
+// utilzamos el bucle for para hacer la tabla
+for (let i = 1; i <= 10; i++) {
+  console.log(nuemero4, "x", i, "=", nuemero1 * i);
+}
+// salto de linea
+console.log("\n");
