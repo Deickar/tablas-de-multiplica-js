e28f938a66ec1b4135bb37807241b43fbdaaa4eb
Deickar <deickar@outlook.com>
2025-06-04 20:16:43 -0600

    se hagregaron lo tablas desapreciodas

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // tabla de multiplica del 1
-let nuemero1 = 1;
+let numero1 = 1;
 console.log("Tabla del 1");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero1, "x", i, "=", nuemero1 * i);
+  console.log(numero1, "x", i, "=", numero1 * i);
 }
 // salto de linea
 console.log("\n");
 
 //tabla de multiplica del 2
-let nuemero2 = 2;
+let numero2 = 2;
 console.log("Tabla del 2");
 // ejemplo con for
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero2, "x", i, "=", nuemero2 * i);
+  console.log(numero2, "x", i, "=", numero2 * i);
 }
+// salto de linea
+console.log("\n");
 
 // tabla de multiplica del 3
 let nuemero3 = 3;
 console.log("Tabla del 3");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero3, "x", i, "=", nuemero1 * i);
+  console.log(nuemero3, "x", i, "=", nuemero3 * i);
 }
 // salto de linea
 console.log("\n");
@@ -31,7 +33,47 @@ let nuemero4 = 4;
 console.log("Tabla del 4");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero4, "x", i, "=", nuemero1 * i);
+  console.log(nuemero4, "x", i, "=", nuemero4 * i);
+}
+// salto de linea
+console.log("\n");
+
+//tabla de multiplica del 5
+let numero5 = 5;
+console.log("Tabla del 5");
+// ejemplo con for
+for (let i = 1; i <= 10; i++) {
+  console.log(numero5, "x", i, "=", numero5 * i);
+}
+// salto de linea
+console.log("\n");
+
+//tabla de multiplica del 6
+let numero6 = 6;
+console.log("Tabla del 6");
+// ejemplo con for
+for (let i = 1; i <= 10; i++) {
+  console.log(numero6, "x", i, "=", numero6 * i);
+}
+// salto de linea
+console.log("\n");
+
+//tabla de multiplicar del 7
+let numero7 = 7;
+console.log("Tabla del 7");
+// ejemplo con for
+for (let i = 1; i <= 10; i++) {
+  console.log(numero7, "x", i, "=", numero7 * i);
+}
+// salto de linea
+console.log("\n");
+
+//tabla de multiplicar del 8
+let numero8 = 8;
+console.log("Tabla del 8");
+// ejemplo con for
+for (let i = 1; i <= 10; i++) {
+  console.log(numero8, "x", i, "=", numero8 * i);
 }
 // salto de linea
 console.log("\n");
@@ -51,7 +93,7 @@ let numero10 = 10;
 console.log("Tabla del 10");
 //utilizamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(numero10, "x", i, "=", numero10 * i );
+  console.log(numero10, "x", i, "=", numero10 * i);
 }
 //salto de linea
 console.log("/n");
