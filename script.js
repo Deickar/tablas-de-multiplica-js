643942c56eb7fa880c79ec33334f4519dc75167b
Deickar <deickar@outlook.com>
2025-06-04 19:08:10 -0600

    se cambio nombre le varible numero1 y numer02

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // tabla de multiplica del 1
-let nuemero1 = 1;
+let numero1 = 1;
 console.log("Tabla del 1");
 // utilzamos el bucle for para hacer la tabla
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero1, "x", i, "=", nuemero1 * i);
+  console.log(numero1, "x", i, "=", numero1 * i);
 }
 // salto de linea
 console.log("\n");
 
 //tabla de multiplica del 2
-let nuemero2 = 2;
+let numero2 = 2;
 console.log("Tabla del 2");
 // ejemplo con for
 for (let i = 1; i <= 10; i++) {
-  console.log(nuemero2, "x", i, "=", nuemero2 * i);
+  console.log(numero2, "x", i, "=", numero2 * i);
 }
 
 // tabla de multiplica del 3
